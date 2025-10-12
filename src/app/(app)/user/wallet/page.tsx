df00dee015932ae6690cecd7888f3b840fd39e7a
f.manoochehri <f.manoochehri@asiaclass.org>
2025-10-12 16:00:11 +0330

    WIP: user wallet page

--- src/app/(app)/user/wallet/page.tsx
+++ src/app/(app)/user/wallet/page.tsx
@@ -1,7 +1,58 @@
+"use client";
+
+import ErrorHandler from "@/components/errors/ErrorHandler";
+import { Balance } from "@/types/wallet";
+import {
+  CircularProgress,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const UserWallet = () => {
-  return <div>UserWallet</div>;
+  const {
+    data: balance,
+    status: balanceStatus,
+    refetch: balanceRefetch,
+  } = useQuery<Balance, Error, Balance>({
+    queryKey: ["wallets", "me"],
+    staleTime: Infinity,
+  });
+
+  return (
+    <Container maxWidth="lg" sx={{ mt: 2 }}>
+      <div className="w-full flex justify-center">
+        {balanceStatus === "pending" ? (
+          <CircularProgress size={60} color="secondary" />
+        ) : balanceStatus === "error" ? (
+          <ErrorHandler onRefetch={balanceRefetch} />
+        ) : (
+          <Typography
+            variant="h4"
+            color="success"
+            textAlign="center"
+            fontWeight={700}
+          >
+            موجودی: {Number(balance.balance).toLocaleString("fa")}
+          </Typography>
+        )}
+      </div>
+      <div className="border border-gray-500 rounded-lg p-4 mt-4 mb-4">
+        <Typography variant="h6">افزایش موجودی</Typography>
+        <Typography variant="body2">
+          برای افزایش موجودی کد RRN تراکنش را اینجا قرار دهید
+        </Typography>
+        <form className="flex gap-2 w-full ">
+          <TextField label={"RRN"} fullWidth size="small" />
+          <TextField label={"مبلغ"} fullWidth size="small" />
+        </form>
+      </div>
+      {/* TODO: Table with pagination */}
+      <Typography>لیست تراکنشهای قبلی</Typography>
+    </Container>
+  );
 };
 
 export default UserWallet;
