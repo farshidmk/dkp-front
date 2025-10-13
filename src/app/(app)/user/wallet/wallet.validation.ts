c87792d5db5005411d3f4a9c84a49276d45e6867
f.manoochehri <f.manoochehri@asiaclass.org>
2025-10-13 13:05:03 +0330

    disable any type check in eslint, fix some validation

--- src/app/(app)/user/wallet/wallet.validation.ts
+++ src/app/(app)/user/wallet/wallet.validation.ts
@@ -3,20 +3,26 @@ import { TransactionType } from "@/types/wallet";
 
 export const WalletFormValidation = z.object({
   type: z.nativeEnum(TransactionType, {
-    errorMap: () => ({ message: "نوع تراکنش نامعتبر است" })
+    error: () => ({ message: "نوع تراکنش نامعتبر است" }),
   }),
   amount: z
-    .string({ required_error: "مبلغ الزامی است" })
+    .string({ error: "مبلغ الزامی است" })
     .min(1, "مبلغ الزامی است")
     .refine((val) => !isNaN(Number(val)), "مبلغ باید عدد باشد")
     .refine((val) => Number(val) >= 1000, "حداقل مبلغ ۱۰۰۰ تومان است")
-    .refine((val) => Number(val) <= 100000000, "حداکثر مبلغ ۱۰۰ میلیون تومان است"),
+    .refine(
+      (val) => Number(val) <= 100000000,
+      "حداکثر مبلغ ۱۰۰ میلیون تومان است"
+    ),
   order_id: z
-    .string({ required_error: "کد پیگیری الزامی است" })
+    .string({ error: "کد پیگیری الزامی است" })
     .min(1, "کد پیگیری الزامی است")
     .refine((val) => !isNaN(Number(val)), "کد پیگیری باید عدد باشد")
     .refine((val) => Number(val) >= 1, "کد پیگیری باید بزرگتر از صفر باشد")
-    .refine((val) => Number(val) <= 999999999, "کد پیگیری نباید بیش از ۹ رقم باشد"),
+    .refine(
+      (val) => Number(val) <= 999999999,
+      "کد پیگیری نباید بیش از ۹ رقم باشد"
+    ),
   description: z
     .string()
     .max(500, "توضیحات نباید بیش از ۵۰۰ کاراکتر باشد")
